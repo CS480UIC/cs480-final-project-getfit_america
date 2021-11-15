994ede2f4c22a0b2fd6d20e0ba2454ac4a9ee9ba
vincemisayah <70535158+vincemisayah@users.noreply.github.com>
2021-11-14 23:30:17 -0600

    adding deleteDB.sql

---
+++ sql/deleteDB.sql
@@ -0,0 +1,9 @@
+-- deletes all the tables of getfiteamerica database
+
+DROP TABLE department;
+DROP TABLE trainer;
+DROP TABLE manager;
+DROP TABLE client;
+DROP TABLE receptionist;
+DROP TABLE program;
+DROP TABLE appointment;
