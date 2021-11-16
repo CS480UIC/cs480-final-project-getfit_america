6858fa545f6599e28b8f4ae2151b6d3f2c98a09f
misayahmelville <misayahmelville@gmail.com>
2021-11-16 02:25:16 -0600

    init

---
+++ src/init/service/initException.java
@@ -0,0 +1,18 @@
+package init.service;
+
+public class initException extends Exception {
+
+	/**
+	 * implement any exception here
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public initException() {
+		super();
+	}
+
+	public initException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/init/service/initService.java
@@ -0,0 +1,45 @@
+package init.service;
+
+import java.util.List;
+
+import user.dao.UserDao;
+import user.domain.User;
+
+/**
+ * logic functions such as register, login
+ * @author Aayush Makharia
+ */
+public class initService {
+	private UserDao userDao = new UserDao();
+	
+	/**
+	 * Register a user
+	 */
+	public void regist(User form) throws initException, ClassNotFoundException, InstantiationException, IllegalAccessException{
+		
+		// check the user name
+		User user = userDao.findByUsername(form.getUsername());
+		if(user.getUsername()!=null && user.getUsername().equals(form.getUsername())) throw new initException("This user name has been registered!");
+		userDao.add(form);
+	}
+	
+	
+	/**
+	 * Login function
+	 */
+	public void login(User form) throws initException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+		User user = userDao.findByUsername(form.getUsername());
+		if(user.getUsername()==null) throw new initException("The user is not in the database");
+		
+		String password = user.getPassword();
+		
+		if(password!=null && !password.equals(form.getPassword()))
+			throw new initException(" The password is not right");
+		
+	}
+	
+	public List<Object> findall() throws InstantiationException, IllegalAccessException, ClassNotFoundException{
+		return userDao.findall();
+		
+	}
+}
